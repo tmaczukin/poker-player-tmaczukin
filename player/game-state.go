ef63bcbf9c4425a63fd858de692ef4f6921b213b
Andrew Slotin <andrew.slotin@gmail.com>
2015-08-03 23:38:05 +0400

    Quote JSON annotations field names

--- player/game-state.go
+++ player/game-state.go
@@ -6,32 +6,32 @@ type GameState struct {
 	SmallBlind int `json:"small_blind"`
 
 	// The amount of the largest current bet from any one player
-	CurrentBuyIn int `json:current_buy_in`
+	CurrentBuyIn int `json:"current_buy_in"`
 
 	// The size of the pot (sum of the player bets)
-	Pot int `json:pot`
+	Pot int `json:"pot"`
 
 	// Minimum raise amount. To raise you have to return at least:
 	//     current_buy_in - players[in_action][bet] + minimum_raise
-	MinimumRaise int `json:minimum_raise`
+	MinimumRaise int `json:"minimum_raise"`
 
 	// The index of the player on the dealer button in this round
 	// The first player is (dealer+1)%(players.length)
-	Dealer int `json:dealer`
+	Dealer int `json:"dealer"`
 
 	// Number of orbits completed. (The number of times the dealer button
 	// returned to the same player.)
-	Orbits int `json:orbits`
+	Orbits int `json:"orbits"`
 
 	// The index of your player, in the players array
-	InAction int `json:in_action`
+	InAction int `json:"in_action"`
 
 	// An array of the players. The order stays the same during the
 	// entire tournament
-	Players []Player `json:players`
+	Players []Player `json:"players"`
 
 	// Finally the array of community cards.
-	CommunityCards []Card `json:community_cards`
+	CommunityCards []Card `json:"community_cards"`
 }
 
 type Player struct {
