2a2e18544734987a55713f51881bbd20eddbf080
Tomasz Maczukin <tomasz@maczukin.pl>
2016-05-20 20:05:35 -0500

    Add some debug

--- player/player.go
+++ player/player.go
@@ -1,17 +1,23 @@
 package player
 
-import "github.com/lean-poker/poker-player-go/leanpoker"
+import (
+	"github.com/lean-poker/poker-player-go/leanpoker"
 
-const VERSION = "20160520200234"
+	"fmt"
+)
+
+const VERSION = "20160520200535"
 
 func BetRequest(state *leanpoker.Game) int {
+	fmt.Printf("BetRequest: %s\n", state)
 	return 0
 }
 
 func Showdown(state *leanpoker.Game) {
-
+	fmt.Printf("Showdown: %s\n", state)
 }
 
 func Version() string {
+	fmt.Printf("VERSION: %s\n", VERSION)
 	return VERSION
 }
