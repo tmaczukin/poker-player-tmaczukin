13dc68d6701bc46d7461f46ac9d0ff80934e29d5
Tomasz Maczukin <tomasz@maczukin.pl>
2016-05-20 23:18:14 -0500

    Remove Godeps

--- Godeps/_workspace/src/github.com/lean-poker/poker-player-go/leanpoker/card.go
+++
@@ -1,9 +0,0 @@
-package leanpoker
-
-type Card struct {
-	// Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
-	Rank string `json:"rank"`
-
-	// Suit of the card. Possible values are: clubs,spades,hearts,diamonds
-	Suit string `json:"suit"`
-}
