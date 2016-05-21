c6a45b50331b07e4703738608a89252407eb82c1
Tomasz Maczukin <tomasz@maczukin.pl>
2016-05-20 23:34:13 -0500

    Update version number

--- player/player.go
+++ player/player.go
@@ -4,7 +4,7 @@ import (
 	"github.com/tmaczukin/poker-player-tmaczukin/leanpoker"
 )
 
-const VERSION = "20160520203933"
+const VERSION = "20160520233413"
 
 func BetRequest(state *leanpoker.Game) int {
 	return 0
